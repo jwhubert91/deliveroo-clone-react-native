9f1a22d3dbb4a138d4d64c2f03b64ea63c7d6d34
James Hubert <jwhubert91@gmail.com>
2023-05-25 11:47:45 -0400

    begin styling homepage

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,13 +1,40 @@
-import { View, Text } from "react-native"
-import React from "react"
+import { View, Text, Image, SafeAreaView } from "react-native"
+import React, { useLayoutEffect } from "react"
+import { useNavigation } from "@react-navigation/native"
+
+// assets
+import FoodIcon from "../assets/burger-icon.png"
+import {
+  UserIcon,
+  ChevronDownIcon,
+  MagnifyingGlassIcon,
+  AdjustmentsHorizontalIcon,
+} from "react-native-heroicons/outline"
 
 // Pick up later @ 00:30:00 exactly (styling home page) - https://www.youtube.com/watch?v=AkEnidfZnCU
 
 const HomeScreen = () => {
+  const navigation = useNavigation()
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      headerShown: false,
+    })
+  }, [])
   return (
-    <View>
-      <Text className="text-red-500">HomeScreen</Text>
-    </View>
+    <SafeAreaView>
+      {/* homescreen */}
+      <View className="flex-row pb-3 items-center mx-4 space-x-2">
+        <Image source={FoodIcon} className="h-7 w-7 p-4 rounded-full" />
+        <View>
+          <Text className="font-bold text-gray-400 text-xs">Deliver Now!</Text>
+          <Text className="font-bold text-xl">
+            Current Location
+            <ChevronDownIcon size={20} color="#00CCBB" />
+          </Text>
+        </View>
+        <UserIcon size={35} color="#00CCBB" />
+      </View>
+    </SafeAreaView>
   )
 }
 
