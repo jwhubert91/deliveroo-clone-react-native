c9b5e2b7ce6f50fc353d192fd676483108a9d61e
James Hubert <jwhubert91@gmail.com>
2023-05-25 12:01:03 -0400

    search box

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,4 +1,4 @@
-import { View, Text, Image, SafeAreaView } from "react-native"
+import { View, Text, Image, SafeAreaView, TextInput } from "react-native"
 import React, { useLayoutEffect } from "react"
 import { useNavigation } from "@react-navigation/native"
 
@@ -21,11 +21,11 @@ const HomeScreen = () => {
     })
   }, [])
   return (
-    <SafeAreaView>
+    <SafeAreaView className="bg-white">
       {/* homescreen */}
-      <View className="flex-row pb-3 items-center mx-4 space-x-2">
+      <View className="flex-row pb-3 items-center mx-4 space-x-2 px-4">
         <Image source={FoodIcon} className="h-7 w-7 p-4 rounded-full" />
-        <View>
+        <View className="flex-1">
           <Text className="font-bold text-gray-400 text-xs">Deliver Now!</Text>
           <Text className="font-bold text-xl">
             Current Location
@@ -33,6 +33,17 @@ const HomeScreen = () => {
           </Text>
         </View>
         <UserIcon size={35} color="#00CCBB" />
+      </View>
+      {/* Search */}
+      <View className="flex-row items-center space-x-2 pb-2 mx-4">
+        <View className="flex-row space-x-2 flex-1 bg-gray-200 p-3">
+          <MagnifyingGlassIcon size={20} color="gray" />
+          <TextInput
+            placeholder="Restaurants and cuisines"
+            keyboardType="default"
+          />
+        </View>
+        <AdjustmentsHorizontalIcon color="#00CCBB" />
       </View>
     </SafeAreaView>
   )
